6b54fb48a5cd53a33396fa46c0b73cba4bb9599b
Kang Yeeun <wjsry1emd@@>
2023-04-24 14:20:53 +0900

    Feat : beakjoon 단계별 문제 body : BufferedReader + StringTokenizer 사용

---
+++ src/Ex4_24b.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
+*
+* readLine()은 한 행을 전부 읽기 때문에 공백 단위로 입력해 준 문자열을 공백 단위로
+* 분리해주어야 문제를 풀 수 있다.
+* */
+public class Ex4_24b {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        System.out.println((A>B) ? ">" : ((A<B) ? "<" : "=="));
+    }
+}
